f668ad243f8c796d243b7a6cc6e5a67d9dcc70a0
DioNiR <dionir90@gmail.com>
2020-03-29 13:30:00 +0300

    new fix

---
+++ config.py
@@ -0,0 +1,39 @@
+from aiogram.utils.markdown import text
+
+MY_ID = 272590789
+
+DB_FILENAME = 'botuploads.db'
+
+TOKEN = '1043413769:AAHV6QU-_N4CKX0rtIClVDLD6Z1wNUUNySc'
+
+PROXY_URL = 'socks5://learn:python@t1.learn.python.ru:1080' 
+
+chat_id = '-1001415606536'
+
+ignore_nicks = ['DioNiR', 'DimkaSnowWhiteBot',]
+#ignore_nicks = []
+
+
+help_message = text(
+    "Доступные команды:\n",
+    "/start - приветствие\n",
+    "/вызывай - попросить бота показать список кого можно вызвать\n",
+    "/вызывай @nick - попросить бота вызывать определенного человека\n",
+    "/хватит - приостанавливает вызов человека",
+    sep="\n"
+)
+
+PHOTOS = [
+    'AgADAgADKawxG5HJ4UsNHS9NdJs-KVjVuQ8ABAEAAwIAA20AA2X3BgABFgQ',
+    'AgADAgAD0KsxGxYl4Uv_HyYbPlXbR7UpwQ4ABAEAAwIAA20AA7xeAAIWBA',
+    'AgADAgADtq0xG5nH4UsJWzlSSCfidRDZug8ABAEAAwIAA20AA7n5BgABFgQ',
+    'AgADAgAD06sxGxYl4UviVIAVnYPRMwABdlwPAAQBAAMCAANtAAP4AAEDAAEWBA',
+    'AgADAgAD1asxGxYl4UvfMbtxd_KGeMHWuQ8ABAEAAwIAA20AA9z1BgABFgQ',
+]
+'''
+RANDOM_PHOTOS [
+    '1',
+]
+'''
+
+UNDER_CONSTRUCTION = 'AgADAgAD_KsxG5HJ6Uvw3pDITRONS_NqXA8ABAEAAwIAA3gAA_wRAwABFgQ'
